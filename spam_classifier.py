fb0d13b40d5c4586801c25c484a03795e0d4f2b8
unknown <angelodepadua2@gmail.com>
2025-06-28 18:41:35 +0800

    added values to ham as 0 and spam as 1

--- spam_classifier.py
+++ spam_classifier.py
@@ -5,6 +5,7 @@ df = pd.read_csv("spam.csv", encoding='latin-1')
 
 df = df[['v1', 'v2']]
 df.columns = ['label', 'message']
+df['label'] = df['label'].map({'ham': 0, 'spam': 1})
 
 print(df.head())
 print(df['label'].value_counts())
