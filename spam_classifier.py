b01c50652172ef6c8d0a7c25346cb4b906808309
unknown <angelodepadua2@gmail.com>
2025-06-28 18:44:27 +0800

    vectorize the data

--- spam_classifier.py
+++ spam_classifier.py
@@ -13,6 +13,10 @@ X_train, X_test, y_train, y_test = train_test_split(
     df['message'], df['label'], test_size=0.2, random_state=42
 )
 
+vectorizer = CountVectorizer()
+X_train_vec = vectorizer.fit_transform(X_train)
+X_test_vec = vectorizer.transform(X_test)
+
 print(df.head())
 print(df['label'].value_counts())
 
