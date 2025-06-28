ea0e8c0e03157a8c7df113ae64d3648fc128f41a
unknown <angelodepadua2@gmail.com>
2025-06-28 18:40:45 +0800

    removed useless colums

--- spam_classifier.py
+++ spam_classifier.py
@@ -3,7 +3,11 @@ import pandas as pd
 # Load the dataset
 df = pd.read_csv("spam.csv", encoding='latin-1')
 
-# Show the first 5 rows
+df = df[['v1', 'v2']]
+df.columns = ['label', 'message']
+
 print(df.head())
+print(df['label'].value_counts())
+
 
 
